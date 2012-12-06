9f3ac0cea1676e5dfe8aff4a55f3affb345ad82e
cake x <c782172004@gmail.com>
2012-12-06 08:41:09 +0800

    update cx_lib bug

--- _core/library/cx_lib.php
+++ _core/library/cx_lib.php
@@ -1,18 +1,21 @@
 <?php
 class cx_lib {
 	var $mCore;
+	var $mConn;
 
 	public function __construct( $_Core = null ) {
 		//parent::__construct();
 		if($_Core == null){
 			global $Core;
 			if($Core){
-				$_Core = &$Core->getDB();
+				$_conn = &$Core->getDB();
+				$_Core = &$Core;
 			}else{
 				echo "ERROR:[cx_lib] don't have Core OR __construct( $_conn ) val adodb loading";
 				exit(0);
 			}
 		}
+		$this->mConn = &$_conn;
 		$this->mCore = &$_Core;
 	}
 }
