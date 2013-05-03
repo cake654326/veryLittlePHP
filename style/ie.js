ec455ddee97557b058c44b0077cea57a497b2067
root <root@localhost.localdomain>
2013-05-03 18:41:44 +0800

    add form_helper and more update

---
+++ style/ie.js
@@ -0,0 +1,14 @@
+(function($)
+{
+    jQuery.fn.setfocus = function()
+    {
+        return this.each(function()
+        {
+            var dom = this;
+            setTimeout(function()
+            {
+                try { dom.focus(); } catch (e) { } 
+            }, 0);
+        });
+    };
+})(jQuery);
